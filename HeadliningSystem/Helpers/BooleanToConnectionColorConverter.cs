343b9d48c54b01ce8d031c27ac9cb01a3cacff3b
sehoon <shkang@cle.vision>
2024-10-24 13:32:37 +0900

    set connect signal

---
+++ HeadliningSystem/Helpers/BooleanToConnectionColorConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using Wpf.Ui.Appearance;
+
+namespace HeadliningSystem.Helpers
+{
+    public class BooleanToConnectionColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not bool isConnected)
+            {
+                throw new ArgumentException("ExceptionEnumToBooleanConverterParameterMustBeAnEnumName");
+            }
+
+            var connectionColor = isConnected ? "Green" : "Red";
+
+            return connectionColor;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not string connectionColor)
+            {
+                throw new ArgumentException("ExceptionEnumToBooleanConverterParameterMustBeAnEnumName");
+            }
+
+            return connectionColor == "Green";
+        }
+    }
+}
--- HeadliningSystem/ViewModels/Pages/DashboardViewModel.cs
+++ HeadliningSystem/ViewModels/Pages/DashboardViewModel.cs
@@ -1,17 +1,76 @@
-﻿namespace HeadliningSystem.ViewModels.Pages
+﻿using System.ComponentModel;
+using System.Security.Cryptography;
+
+namespace HeadliningSystem.ViewModels.Pages
 {
-    public partial class DashboardViewModel : ObservableObject
+    public partial class DashboardViewModel : ObservableObject, INotifyPropertyChanged
     {
         [ObservableProperty]
         private int _counter = 0;
-
         [ObservableProperty]
         private int _pageWidth;
+
+        private bool _isCameraConnected = false;
+        private bool _isPlcConnected = false;
+        private bool _isHyundaiRobotConnected = false;
+        private bool _isFanucRobotConnected = false;
+
+        public bool IsCameraConnected
+        {
+            get => _isCameraConnected;
+            set
+            {
+                _isCameraConnected = value;
+                OnPropertyChanged(nameof(IsCameraConnected));
+            }
+        }
+
+        public bool IsPlcConnected
+        {
+            get => _isPlcConnected;
+            set
+            {
+                _isPlcConnected = value;
+                OnPropertyChanged(nameof(IsPlcConnected));
+            }
+        }
+
+        public bool IsHyundaiRobotConnected
+        {
+            get => _isHyundaiRobotConnected;
+            set
+            {
+                _isHyundaiRobotConnected = value;
+                OnPropertyChanged(nameof(IsHyundaiRobotConnected));
+            }
+        }
+
+        public bool IsFanucRobotConnected
+        {
+            get => _isFanucRobotConnected;
+            set
+            {
+                _isFanucRobotConnected = value;
+                OnPropertyChanged(nameof(IsFanucRobotConnected));
+            }
+        }
 
         [RelayCommand]
         private void OnCounterIncrement()
         {
             Counter++;
+        }
+
+        [RelayCommand]
+        private void OnConnectCamera()
+        {
+            IsCameraConnected = !IsCameraConnected;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
     }
 }
